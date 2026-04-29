58176e2fa9f73a1a44c440cf5117528f99615320
Alex Lledo <alexlledo@me.com>
2026-04-29 21:04:02 +0200

    bug arreglado: forzar renderizado dinámico en páginas de autenticación

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -3,6 +3,8 @@ import { redirect } from "next/navigation";
 import { AuthForm } from "@/components/forms/auth-form";
 import { getOptionalUser } from "@/lib/supabase/auth";
 
+export const dynamic = "force-dynamic";
+
 export default async function RegisterPage() {
   const user = await getOptionalUser();
 
